459b60b3604c81738efd87b70dded7622bcbdcc8
igoryarmolyuk <igor.yarmolyuk00@gmail.com>
2024-09-30 19:34:08 +0300

    first commit

---
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -0,0 +1,29 @@
+package org.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/demo/hello-view.fxml"));
+        Parent root = loader.load();
+
+        AnchorPane anchorPane = (AnchorPane) root;
+
+        Scene scene = new Scene(anchorPane, 600, 400);
+
+        primaryStage.setTitle("Resizable Window Example");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
